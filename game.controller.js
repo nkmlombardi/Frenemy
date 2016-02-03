35ec96c3ef5a976176a79d5d7804635cdb4f6710
Nick Lombardi <nicklombardi1992@gmail.com>
2016-02-03 16:15:37 -0500

    Create game.controller.js

---
+++ game.controller.js
@@ -0,0 +1,23 @@
+var gameController = function(game, players) {
+    var game = this;
+    var players = players;                          // array
+    var timeout = this.GameType.timeout;            // int
+    var rounds = new array[this.GameType.rounds];   // array
+    var phases = new array[this.GameType.phases];   // array
+
+    while (players.count >= GameType.endGamePlayerCount) {
+        if (timeout <= (timeout * .80)) {
+            game.openVoting();
+            game.decrementPhase();
+        }
+
+        if (timeout == 0) {
+            var losers = votes.highestPlayers();    // returns array
+            game.kickLosers(losers);
+            game.decrementPhase();
+        }
+
+        game.resetPhases();
+        game.decrementRound();
+    }
+};
